487320b870d2bde240f2604f61d4a95aac541485
BOBS\Fabricio.Pereira <Fabricio.pereira@bffc.com.br>
2018-09-14 17:31:45 -0300

    Inclusão do tabletop e retirada dos dados via spreadsheets do google

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
-"use strict";
+'use strict'
 
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var sass = require('gulp-sass')
+var rename = require('gulp-rename')
 
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create()
 
-var maps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var htmlreplace = require('gulp-html-replace');
-var uglify = require('gulp-uglify-es').default;
+var maps = require('gulp-sourcemaps')
+var concat = require('gulp-concat')
+var htmlreplace = require('gulp-html-replace')
+var uglify = require('gulp-uglify-es').default
 
-var imagemin = require('gulp-imagemin');
+// var cache = require('gulp-cache')
+var imagemin = require('gulp-imagemin')
+var imageminPngquant = require('imagemin-pngquant')
+var imageminZopfli = require('imagemin-zopfli')
+var imageminMozjpeg = require('imagemin-mozjpeg') //  need to run 'brew install libpng'
 
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
-var uncss = require('postcss-uncss');
-var pxtorem = require('postcss-pxtorem');
-var critical = require('critical').stream;
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
+var uncss = require('postcss-uncss')
+var pxtorem = require('postcss-pxtorem')
+var critical = require('critical').stream
 
-var log = require('fancy-log');
+var log = require('fancy-log')
 
 var srcPath = {
 	styles: 'src/assets/css',
@@ -39,49 +43,72 @@ var distPath = {
 	fonts: 'dist/assets/fonts'
 }
 
-//Concatena os scripts globais pra dev (Mantém sourcemap pra desenvolvimento)
-gulp.task('devScripts', function() {
+// Concatena os scripts globais pra dev (Mantém sourcemap pra desenvolvimento)
+gulp.task('devScripts', function () {
 	return gulp.src([
-		//Os scripts que são concatenados são somente os comuns a todas as páginas
-		//Vendors (libraries externas)
-		//ex: `${srcPath.scripts}/vendor/jquery-3.2.1.slim.min.js`,
-	])
-	.pipe(maps.init())
-	.pipe(concat('main.js'))
-	.pipe(maps.write('./'))
-	.pipe(gulp.dest(`${srcPath.scripts}`))
-	.on('error', function(err) { log.error(err.message); })
-	.pipe(browserSync.stream())
+			// Os scripts que são concatenados são somente os comuns a todas as páginas
+			`${srcPath.scripts}/modules/*.js`
+
+			// Vendors (libraries externas)
+			// ex: `${srcPath.scripts}/vendor/jquery-3.2.1.slim.min.js`,
+		])
+		.pipe(maps.init())
+		.pipe(concat('main.js'))
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest(`${srcPath.scripts}`))
+		.on('error', function (err) {
+			log.error(err.message)
+		})
 })
 
-//Prepara os scripts globais pra produção
-gulp.task('buildGlobalScripts', function() {
-	return gulp.src([
-		//Os scripts que são concatenados são somente os comuns a todas as páginas
-		//Vendors (libraries externas)
-		//ex: `${srcPath.scripts}/vendor/jquery-3.2.1.slim.min.js`,
-	])
-	.pipe(concat('main.js'))
-	.pipe(uglify())
-	.pipe(rename('main.min.js'))
-	.on('error', function(err) { log.error(err.message); })
-	.pipe(gulp.dest(`${distPath.scripts}`))
+gulp.task('js-reload', ['devScripts'], done => {
+	browserSync.reload()
+	done()
 })
 
-//Por Local scripts quero dizer scripts que só são usados em uma página
-gulp.task('buildLocalScripts', function() {
+// Prepara os scripts globais pra produção
+gulp.task('buildGlobalScripts', function () {
+	return gulp.src([
+			// Os scripts que são concatenados são somente os comuns a todas as páginas
+			`${srcPath.scripts}/modules/*.js`
+			// Vendors (libraries externas)
+			// ex: `${srcPath.scripts}/vendor/jquery-3.2.1.slim.min.js`,
+		])
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.on('error', function (err) {
+			log.error(err.message)
+		})
+		.pipe(gulp.dest(`${distPath.scripts}`))
+})
+
+// Por Local scripts quero dizer scripts que só são usados em uma página
+gulp.task('buildLocalScripts', function () {
 	return gulp.src([`${srcPath.scripts}/pages/*.js`])
-	.pipe(uglify())
-	.on('error', function(err) { log.error(err.message); })
-	.pipe(gulp.dest(`${distPath.scripts}/pages`))
+		.pipe(uglify())
+		.on('error', function (err) {
+			log.error(err.message)
+		})
+		.pipe(gulp.dest(`${distPath.scripts}/pages`))
 })
 
-//Compila o sass e processa com o postcss para o ambiente de dev
-gulp.task('devCss', function() {
+gulp.task('buildVendorScripts', function () {
+	return gulp.src([`${srcPath.scripts}/vendors/**/*.js`])
+		.pipe(uglify())
+		.on('error', function (err) {
+			log.error(err.message)
+		})
+		.pipe(gulp.dest(`${distPath.scripts}/vendors`))
+})
+
+// Compila o sass e processa com o postcss para o ambiente de dev
+gulp.task('devCss', function () {
 	var processors = [
-		autoprefixer,
-		pxtorem,
-	];
+		autoprefixer({
+			grid: false
+		}),
+		pxtorem
+	]
 
 	return gulp.src(`${srcPath.styles}/main.scss`)
 		.pipe(maps.init())
@@ -89,15 +116,19 @@ gulp.task('devCss', function() {
 		.pipe(postcss(processors))
 		.pipe(maps.write(`./`))
 		.pipe(gulp.dest(`${srcPath.styles}`))
-		.on('error', function(err) { log.error(err.message); })
+		.on('error', function (err) {
+			log.error(err.message)
+		})
 		.pipe(browserSync.stream())
 })
 
-//Prepara o CSS pra produção
-//Já tem o padrão pra remover código do Bootstrap
-gulp.task('buildCss', ['renameSources', 'buildLocalScripts', 'buildGlobalScripts'], function() {
+// Prepara o CSS pra produção
+// Já tem o padrão pra remover código do Bootstrap
+gulp.task('buildCss', ['renameSources', 'buildLocalScripts', 'buildGlobalScripts'], function () {
 	var processors = [
-		autoprefixer,
+		autoprefixer({
+			grid: false
+		}),
 		pxtorem,
 		cssnano,
 		uncss({
@@ -105,77 +136,119 @@ gulp.task('buildCss', ['renameSources', 'buildLocalScripts', 'buildGlobalScripts
 			htmlroot: `./dist`,
 			ignore: [
 				/\.js-/, '.hide-input-content', /^\.input/, /^\.uncss-/,
-				/\.fade/, /\.modal/, '.affix', /\.tooltip/, /\.popover/, /\.collaps/,
+				/\.fade/, /\.modal/, '.affix', /\.tooltip/, /\.popover/, /\.collaps/, /^\.swiper-/, /^\.fa-/,
+				/^\.form-/
 			]
 		})
-	];
+	]
 
 	return gulp.src(`${srcPath.styles}/main.scss`)
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass().on('buildCss error: ', sass.logError))
 		.pipe(postcss(processors))
-		.pipe(rename('main.css'))
-		.on('error', function(err) { log.error(err.message); })
+		.on('buildCss error: ', function (err) {
+			log.error(err.message)
+		})
 		.pipe(gulp.dest(`${distPath.styles}`))
 })
 
-//Task de watch, executa tasks de dev quando scripts e css são alterados
-gulp.task('watchFiles', function() {
+// Task de watch, executa tasks de dev quando scripts e css são alterados
+gulp.task('watchFiles', function () {
 	gulp.watch(`${srcPath.styles}/**/*.scss`, ['devCss'])
 	gulp.watch(`${srcPath.scripts}/**/*.js`, ['devScripts'])
+	gulp.watch(`${srcPath.pages}/*.html`).on('change', browserSync.reload)
 })
 
-//Troca o caminho dos scripts no html para refletir a estrutura da pasta de produção (dist)
-gulp.task('renameSources', function(){
+// Troca o caminho dos scripts no html para refletir a estrutura da pasta de produção (dist)
+gulp.task('renameSources', function () {
 	return gulp.src([`${srcPath.pages}/*.html`])
-	.pipe(htmlreplace({
-		'globaljs': `assets/js/main.min.js`,
-		'css': `assets/css/main.css`,
-	}))
-	.on('error', function(err) { log.error(err.message); })
-	.pipe(gulp.dest(`${distPath.pages}`))
+		.pipe(htmlreplace({
+			'globaljs': `assets/js/main.js`,
+			'css': `assets/css/main.css`
+		}))
+		.on('error', function (err) {
+			log.error(err.message)
+		})
+		.pipe(gulp.dest(`${distPath.pages}`))
 })
 
-//Otimiza as imagens (não comprime, apenas reduz o tamanho de outras formas sem perda)
-//Só funciona para jpg,gif,png,svg. Quaisquer outros tipos de arquivos não pararão na pasta de prod
-gulp.task('buildImages', function(){
+// Otimiza as imagens (não comprime, apenas reduz o tamanho de outras formas sem perda)
+// Só funciona para jpg,gif,png,svg. Quaisquer outros tipos de arquivos não pararão na pasta de prod
+gulp.task('buildImages', ['renameSources', 'buildCss', 'buildGlobalScripts', 'buildLocalScripts', 'buildVendorScripts', 'copyFonts', 'criticalCss'], function () {
 	return gulp.src(`${srcPath.images}/**/*.*`)
-		.pipe(imagemin())
-		.on('error', function(err) { log.error(err.message); })
+		.pipe(imagemin([
+			// png
+			imageminPngquant({
+				speed: 1,
+				quality: 60 // lossy settings
+			}),
+			imageminZopfli({
+				more: true,
+				iterations: 50
+			}),
+			// svg
+			imagemin.svgo({
+				plugins: [{
+					removeViewBox: false
+				}]
+			}),
+			// jpg lossless
+			imagemin.jpegtran({
+				progressive: true
+			}),
+			// jpg very light lossy, use vs jpegtran
+			imageminMozjpeg({
+				quality: 60
+			})
+		], {
+			verbose: true
+		}))
+		.on('error', function (err) {
+			log.error(err.message)
+		})
 		.pipe(gulp.dest(`${distPath.images}`))
 })
 
-//Copia as fontes
-gulp.task('copyFonts', function() {
-	gulp.src(`${srcPath.fonts}/*.{woff,woff2}`)
+// Copia as fontes
+gulp.task('copyFonts', function () {
+	gulp.src(`${srcPath.fonts}/**/*.{woff2,woff,ttf,svg}`)
 		.pipe(gulp.dest(`${distPath.fonts}`))
 })
 
-//Pega CSS crítico (acima do fold) e o deixa inline nos HTMLs
-gulp.task('criticalCss', ['renameSources', 'buildCss'], function() {
+// Pega CSS crítico (acima do fold) e o deixa inline nos HTMLs
+gulp.task('criticalCss', ['renameSources', 'buildCss'], function () {
 	return gulp.src(`${distPath.pages}/*.html`)
 		.pipe(critical({
 			base: `${distPath.root}`,
 			inline: true,
 			css: [`${distPath.styles}/main.css`],
-			dimensions: [
-			{
-				with:320,
-				height:480
-			}
-			],
-			ignore: ['@font-face',/url\(/]
+			dimensions: [{
+				with: 320,
+				height: 568
+			}],
+			ignore: ['@font-face', /url\(/]
 		}))
-		.on('error', function(err) { log.error(err.message); })
+		.on('error', function (err) {
+			log.error('criticalcss: ' + err.message)
+		})
 		.pipe(gulp.dest(`${distPath.root}`))
 })
 
-//Task de deploy. Prepara tudo para a produção, rode essa aqui
-gulp.task('deploy', ['renameSources', 'buildImages', 'buildCss', 'buildGlobalScripts', 'buildLocalScripts', 'copyFonts', 'criticalCss'], function(){
-})
+// Task de deploy. Prepara tudo para a produção, rode essa aqui
+gulp.task('deploy',
+	['renameSources', 'buildImages', 'buildCss',
+		'buildGlobalScripts', 'buildLocalScripts', 'buildVendorScripts',
+		'copyFonts', 'criticalCss'
+	],
+	function () {})
 
-//Prepara o browsersync (server de desenvolvimento com live reload)
-gulp.task('dev', ['watchFiles'], function() {
+// Prepara o browsersync (server de desenvolvimento com live reload)
+gulp.task('dev', ['watchFiles'], function () {
 	browserSync.init({
-		server: `${srcPath.pages}`
+		server: {
+			baseDir: `${srcPath.pages}`
+		},
+		ghostMode: false
 	})
+
+	gulp.watch(`${srcPath.scripts}/**/*.js`, ['js-reload'])
 })
